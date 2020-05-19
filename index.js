fa6961965708779f6dd27873ecf7757f726e1ced
Zainab Saud <zai99nb@gmail.com>
2020-05-19 00:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,21 @@ function greet(timeString) {
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
 function displayMessage() {
+<<<<<<< HEAD
   let greeting = document.getElementById("greeting");
   console.log(greeting);
   greeting.innerText ="Good Morning";
   greeting.innerText = "Good Afternoon";
   greeting.innerText = "TEST";
 }
+=======
+   let testContent = "TEST";
+   let content = document.getElementById("greeting ");
+  if (content = testContent) {
+    console.log ("Good Morning");
+    console.log ("Good Afternoon");
+    console.log ("Good Evening");
+  }
+  
+}
+>>>>>>> 545c0146b3153592626043838c3282465911f5b6
